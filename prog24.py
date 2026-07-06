2debf7d860681f2d3f22c30f8ae0599dbc2f2967
GhDuarte <gustavohvdv@gmail.com>
2026-07-06 20:54:59 -0300

    prog24.py

---
+++ prog24.py
@@ -0,0 +1,8 @@
+a=float(input("digete o primeiro valor:"))
+b=float(input("digete o segundo valor:"))
+if a>b:
+    print("o maior valor é",a)
+elif b>a:
+    print("o maior valor é",b)
+else:
+    print("os dois valores são iguais.")
